c260274f7a75cdfbb097a63411da3620b1a0c38d
meiyungzs <meiyungzs@gmail.com>
2018-06-19 11:23:22 +0800

    first commit

---
+++ src/pages/test/params.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="test">
+    <h1>{{ msg }}</h1>
+    <router-link to="/test">前往test页面</router-link>
+    <h2 @click="showParams">点击打印id</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'params',
+  data () {
+    return {
+      msg: "展示id",
+    }
+  },
+  created: function() {
+      console.log(this.$route.query)
+      console.log(this.$route.params)
+  },
+  methods:{
+    showParams(){
+      console.log(this.$route.query)
+      console.log(this.$route.params)
+    }
+  }
+}
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/store/types.js
@@ -0,0 +1,11 @@
+// popup loading
+export const SHOW_POPUP_LOADING = 'SHOW_POPUP_LOADING'
+export const HIDE_POPUP_LOADING = 'HIDE_POPUP_LOADING'
+
+// popup modal
+export const MODAL_SHOW = 'MODAL_SHOW'
+export const MODAL_HIDE = 'MODAL_HIDE'
+
+// login
+export const LOGIN_IN = 'LOGIN_IN'
+export const LOGIN_OUT = 'LOGIN_OUT'
