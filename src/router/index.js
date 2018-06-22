bafeed2a4383ef593de06de86544dcbf9f14eeaf
meiyungzs <meiyungzs@gmail.com>
2018-06-22 14:10:11 +0800

    22

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  base: '/dist/',
+  base: '/vuesubdist/',
   scrollBehavior: () => ({ y: 0 }),
   routes: [{
     path: '/hello',
